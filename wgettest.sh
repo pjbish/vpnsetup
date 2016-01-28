1badeb77bb3d351a8843d881f40c7ecf81647909
pjbish <pjbish@users.noreply.github.com>
2016-01-29 10:10:24 +1100

    Test for wget debug

---
+++ wgettest.sh
@@ -0,0 +1,6 @@
+#VPN 2 - Setup PPTP Server
+wget=/usr/bin/wget
+$wget https://github.com/pjbish/vpnsetup/raw/master/pptpd.sh
+#/bin/sh pptpd.sh
+#wget https://github.com/pjbish/vpnsetup/raw/master/pptpd.sh
+sh pptpd.sh
