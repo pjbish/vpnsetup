fbf02fd5c18a7d35f5961273afe50279f776f062
pjbish <pjbish@users.noreply.github.com>
2016-05-13 16:16:14 +1000

    Update pptpd.sh

--- pptpd.sh
+++ pptpd.sh
@@ -6,6 +6,8 @@
 echo "nospoof on" >> /etc/host.conf
 
 echo "starting PPTPD install"
+apt-get update 
+apt-get upgrade 
 apt-get install pptpd -y
 echo "PPTPD install complete"
 
@@ -76,7 +78,6 @@ iptables -I FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 #EOF
 
 #chmod +x /etc/network/if-pre-up.d/iptablesload
-
 #apt-get install denyhosts fail2ban
 
 echo "15.1 workaround"
@@ -85,6 +86,27 @@ echo "15.1 workaround"
 #
 echo "installing denyhosts & fail2ban"
 apt-get install denyhosts fail2ban
+
+#FTP
+apt-get install vsftpd
+rm /etc/vsftpd.conf
+cat > /etc/vsftpd.conf <<EOF
+listen=YES
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+dirmessage_enable=YES
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+secure_chroot_dir=/var/run/vsftpd/empty
+pam_service_name=vsftpd
+rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+secure_chroot_dir=/var/run/vsftpd/empty
+pam_service_name=vsftpd
+rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+EOF
 
 #install PPTPD monitor
 wget https://github.com/pjbish/pptpd-monitor/raw/master/src/pptpd-monitor.py
